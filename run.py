7df32ddfb5d10fbf8eee674b5aed0f2b6302b00b
Riman Farhood <riman.farhood@gmail.com>
2023-09-27 15:33:24 +0000

    Create get_user_data function to get users inputs

--- run.py
+++ run.py
@@ -12,3 +12,16 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Crime-statistics-Swe')
+
+
+def get_user_data():
+    """
+    Get user input for year and crime offense
+    """
+    while True:
+        print('Enter the year you would like statistics for,')
+        print('between the years 2012 - 2022.')
+        print('Example: 2019\n')
+
+        global input_year
+        input_year = input("Enter year here")
